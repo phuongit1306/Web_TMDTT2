3c8b2ccfa4ae32529039813285a07e358f87982e
Phuong <phuongit1306@gmail.com>
2025-04-07 03:31:22 +0700

    connect data & admincp

---
+++ admincp/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admincp</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h3>Welcome to Admincp</h3>
+    <?php
+        include("modules/header.php");
+        include("modules/menu.php");
+        include("modules/main.php");
+        include("modules/footer.php");
+        ?>
+</body>
+</html>
