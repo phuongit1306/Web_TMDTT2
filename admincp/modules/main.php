5d01d0329975bb2bd9767ed5cc2619b5452a9a82
Phuong <phuongit1306@gmail.com>
2025-04-07 18:26:36 +0700

    Quanlydanhmucsp b3

--- admincp/modules/main.php
+++ admincp/modules/main.php
@@ -1,17 +1,22 @@
 <div class="clear"></div>
 <div class="main">
                 <?php
-                    if(isset($_GET['action']))
+                    if(isset($_GET['action']) && $_GET['query'])
                     {
                         $tam = $_GET['action'];
+                        $query = $_GET['query'];
                     }
                     else{
                         $tam = '';
+                        $query = '';
                     }
-                    if($tam == 'quanlydanhmucsanpham')
+                    if($tam== 'quanlydanhmucsanpham' && $query=='them')
                     {
                         include("modules/quanlydanhmucsp/them.php");
                         include("modules/quanlydanhmucsp/lietke.php");
+                    }elseif($tam== 'quanlydanhmucsanpham' && $query=='sua')
+                    {
+                        include("modules/quanlydanhmucsp/sua.php");
                     }else
                     {
                         include("modules/dashboard.php");
