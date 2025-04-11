6ac195a7c2fdff61fa589c2dc270a60a1786ece7
Phuong <phuongit1306@gmail.com>
2025-04-11 08:27:20 +0700

    Pages

--- pages/main/index.php
+++ pages/main/index.php
@@ -1,5 +1,16 @@
 <?php
-        $sql_pro = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc ORDER BY tbl_sanpham.id_sanpham DESC LIMIT 25";
+    if(isset($_GET['trang'])){
+        $page = $_GET['trang'];
+    }else{
+        $page = 1;
+    }
+    if($page == '' || $page == 1){
+        $begin = 0;
+    }else{
+        $begin = ($page*3)-3;
+    }
+
+        $sql_pro = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc ORDER BY tbl_sanpham.id_sanpham DESC LIMIT $begin,3";
         $query_pro = mysqli_query($mysqli, $sql_pro);
 
 ?>
@@ -21,3 +32,42 @@
                     }
                     ?>
                 </ul>
+                <div style="clear:both;"></div>
+                <style type="text/css">
+                    ul.list_trang{
+                        padding: 0;
+                        margin: 0;
+                        list-style: none;
+                    }
+                    ul.list_trang li{
+                        float: left;
+                        padding: 5px 13px;
+                        margin: 5px;
+                        background: burlywood;
+                        display: block;
+                    }
+                    ul.list_trang li a {
+                        color: #000;
+                        text-align: center;
+                        text-decoration: none;
+                    }
+                    </style>
+                    <?php
+                        $sql_trang = mysqli_query($mysqli, "SELECT * FROM tbl_sanpham");
+                        $row_count = mysqli_num_rows($sql_trang);
+                        $trang = ceil($row_count/3);
+                    ?>
+                    <p>Trang hiện tại: <?php echo $page ?>/<?php echo $trang ?> </p>
+
+
+                    <ul class="list_trang">
+                        <?php
+                        for($i=1; $i<=$trang;$i++){
+                        ?>
+                            <li <?php if($i==$page){echo 'style="background:red;"';}else{echo '';} ?>><a href="index.php?trang=<?php echo $i ?>"><?php echo $i ?></a></li>
+                        <?php
+                        }
+                        ?>
+                    </ul>
+
+               
